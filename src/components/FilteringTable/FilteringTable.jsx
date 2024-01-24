773c572aa7483d1311ee95917ddfeab711ad8e4a
Ram Sanjay <ramsanjaydev08@gmail.com>
2024-01-24 17:29:27 +0530

    added

--- src/components/FilteringTable/FilteringTable.jsx
+++ src/components/FilteringTable/FilteringTable.jsx
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import { Form } from "react-bootstrap";
 import { InputGroup } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {data} from "./data"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data } from "./data";
 
 const FilteringTable = () => {
+  const [search, setSearch] = useState("");
+  console.log(search);
+
+  const handleSearchInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredData = useMemo(()=> {
+    return data.filter((item) => {
+      return search.toLowerCase() === "" ? item : item.first_name.toLowerCase().includes(search)
+    })
+  },[data,search])
+
   return (
     <Container>
       <h1 className="mt-4 text-center text-3xl font-bold">Contact Details</h1>
       <Form>
         <InputGroup className="my-3">
-          <Form.Control placeholder="Search Items"/>
+          <Form.Control
+            placeholder="Search Items"
+            onChange={handleSearchInput}
+          />
         </InputGroup>
       </Form>
       <Table striped bordered hover>
         <tbody>
-        <tr>
-          <td>First Name</td>
-          <td>Last Name</td>
-          <td>Email</td>
-          <td>Phone</td>
-        </tr>
-        {data.map(({id,first_name,last_name,email,phone})=> {
-          return <tr key={id}>
-            <td>{first_name}</td>
-            <td>{last_name}</td>
-            <td>{email}</td>
-            <td>{phone}</td>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Phone</td>
           </tr>
-        })}
+          {filteredData.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </Container>
